affe2c19684ce8d06803cc5939ed24278ce8f7f2
Radu Rebeja <r.rebeja@student.rug.nl>
2021-04-08 19:54:34 +0200

    ain.c

---
+++ cmake-build-debug/CMakeFiles/week5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week5.dir/dijkstra.c.o"
+  "CMakeFiles/week5.dir/main.c.o"
+  "CMakeFiles/week5.dir/priorityh.c.o"
+  "week5"
+  "week5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/week5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
